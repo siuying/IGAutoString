bfc9f9b782fbc6fddf4015fb904e07c25e5fddc3
Francis Chong <francis@ignition.hk>
2013-05-07 22:53:09 +0800

    Instead of use NSString to convert string, use iconv to convert string into UTF-8 first. - use iconv to convert string - ignore conversion error - update UniversalDetector to get encoding as string

--- IGAutoString/IGAutoString.m
+++ IGAutoString/IGAutoString.m
@@ -11,54 +11,92 @@
 #import <iconv.h>
 #import <string.h>
 
+// convert encoding
+char* ig_convert_encoding(const char *src, const char *tocode, const char *fromcode)
+{
+    iconv_t cd;
+    size_t in, out, len, err;
+    char *dest, *outp, *inp = (char *) src;
+    
+    cd = iconv_open(tocode, fromcode);
+    if (cd < 0)
+        return NULL;
+    
+    int argument = 1;
+    iconvctl(cd, ICONV_SET_DISCARD_ILSEQ, &argument);
+    
+    in = strlen(src);
+    out = len = in * 3 / 2 + 1;
+    outp = dest = (char *) malloc(len);
+    
+    while (in > 0) {
+        err = iconv(cd, &inp, &in, &outp, &out);
+        if (err == (size_t)(-1)) {
+            if (errno == E2BIG) {
+                size_t used = outp - dest;
+                len *= 2;
+                
+                char *newdest = (char *) realloc(dest, len);
+                if (!newdest)
+                    break;
+                
+                dest = newdest;
+                outp = dest + used;
+                out = len - used - 1;
+            } else {
+                break;
+            }
+        }
+    }
+    
+    if (outp)
+        *outp = '\0';
+    
+    iconv_close(cd);
+    return dest;
+}
+
 @implementation IGAutoString
 
 -(id) init {
     self = [super init];
     if (self) {
-        self.shouldTryBig5Variant = YES;
         self.detector = [[UniversalDetector alloc] init];
     }
     return self;
 }
 
 -(NSString*) stringWithData:(NSData*)data {
-    CFStringEncodings encoding = [self.detector detectEncoding:data];
+    NSString* encoding = [self.detector encodingAsStringWithData:data];
     NSString* encoded;
 
-    // if encoding invalid/ascii, try UTF-8 anyway
-    if (encoding == kCFStringEncodingInvalidId) {
-        return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    // by default, use iconv
+    if (encoding) {
+        encoded = [IGAutoString stringWithData:data encoding:encoding];
     }
     
-    // if encoding is Big5, try possible variants first
-    if (self.shouldTryBig5Variant && encoding == kCFStringEncodingBig5) {
-        if (!encoded) {
-            encoded = [[NSString alloc] initWithData:data encoding:CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingBig5_HKSCS_1999)];
-        }
-        if (!encoded) {
-            encoded = [[NSString alloc] initWithData:data encoding:CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingBig5_E)];
-        }
+    // as fallback, guess it as UTF-8
+    if (!encoded) {
+        encoded = [NSString stringWithCString:[data bytes] encoding:NSUTF8StringEncoding];
     }
 
-    // by default, use whatever encoding we guessed
+    // if UTF-8 failed, try ASCII
     if (!encoded) {
-        NSStringEncoding nsEncoding = CFStringConvertEncodingToNSStringEncoding(encoding);
-        encoded = [[NSString alloc] initWithData:data encoding:nsEncoding];
-    }
-
-    // if all failed, try UTF8 anyway
-    if (!encoded) {
-        encoded = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        encoded = [NSString stringWithCString:[data bytes] encoding:NSASCIIStringEncoding];
     }
 
     return encoded;
 }
 
 +(NSString*) stringWithData:(NSData *)data encoding:(NSString*)encoding {
-    // TODO implement me
-
-    return nil;
+	char *buf = ig_convert_encoding((char*)[data bytes], "UTF-8", [encoding UTF8String]);
+    if (buf) {
+        NSString* result = [NSString stringWithCString:buf encoding:NSUTF8StringEncoding];
+        free(buf);
+        return result;
+    } else {
+        return nil;
+    }
 }
 
 +(NSString*) stringWithData:(NSData*)data {
